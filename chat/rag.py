1ffb8568f0799a1ddb9e77061eaa8b6bcd95f103
sh.jung <sh.jung@okestro.com>
2023-08-25 17:07:01 +0900

    [FIX] separate the func 'get_sentence'

--- chat/rag.py
+++ chat/rag.py
@@ -1,18 +1,25 @@
 # Retrieval Augmented Generation 코드를 여기에 작성합니다.
 
+from typing import List
 from langchain import OpenAI, SQLDatabase
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain_experimental.sql import SQLDatabaseChain
+from langchain.chains.router import MultiPromptChain
+from langchain.llms import OpenAI
+from langchain.chains import ConversationChain
+from langchain.chains.llm import LLMChain
+from langchain.prompts import PromptTemplate
+from langchain.chains.router.llm_router import LLMRouterChain, RouterOutputParser
+from langchain.chains.router.multi_prompt_prompt import MULTI_PROMPT_ROUTER_TEMPLATE
+from pydantic import BaseModel
+
+import my_settings
 
 
-class RAG:
-    def __init__(self, query, db_info):
-        self.query = query
-        self.db_info = db_info
+class RAG(BaseModel) :
+    query: str
 
-    def get_SQLDatabaseChain_result(self):
-
-        prompt_template = """
+    SQL_prompt_template:str = """
         Given an input question, first create a syntactically correct postgresql query to run, then look at the results of the query and return the answer.
         Use the following format:
 
@@ -21,13 +28,75 @@ class RAG:
         SQLResult: Result of the SQLQuery
         Answer: Final answer here
 
-        {query}
+        {query}"""
+
+    dummy_prompt_template:str = """
+        Tell me a joke with following words.
+         
+        {query}"""
+
+    prompt_infos:List[dict] = [
+        {
+            "name" : "db_chain",
+            "description" : "Good for answering questions about instance billing or instance usages",
+            "prompt_template" : SQL_prompt_template
+        },
+        {
+            "name": "dummy_chain",
+            "description": "Good for answering questions about making a joke",
+            "prompt_template": dummy_prompt_template
+        }
+    ]
+
+    def get_sentence(self):
+
+        SQL_db = SQLDatabase.from_uri(my_settings.RAG_DB_INFO)
+        llm = OpenAI(streaming=True, callbacks=[StreamingStdOutCallbackHandler()], temperature=0, verbose=True)
+        db_chain = SQLDatabaseChain.from_llm(llm, SQL_db, verbose=True)
+
+        return db_chain(self.query)['result']
+
+
+    def get_sentence_from_MultiPromptChain(self):
+        """
+        MultiPromptChain을 통해서 similarity 기반의 의도 분류기 역할을 수행하고자 한다.
+
+        이슈 사항으로는, MultiPromptChain에서 SQLDatabaseChain은 지원하지 않는 듯..
+        단순 LLMChain만 지원하는 것으로 보여지니 확인이 필요함.
         """
 
-        prompt = prompt_template.format(query=self.query)
-
-        db = SQLDatabase.from_uri(self.db_info)
         llm = OpenAI(streaming=True, callbacks=[StreamingStdOutCallbackHandler()], temperature=0, verbose=True)
 
-        db_chain = SQLDatabaseChain.from_llm(llm, db, verbose=True)
-        return db_chain(prompt)['result']
+        destination_chains = {}
+
+        SQL_db = SQLDatabase.from_uri(my_settings.RAG_DB_INFO)
+        db_chain_prompt = PromptTemplate(template=self.SQL_prompt_template, input_variables=["query"])
+        db_chain = SQLDatabaseChain.from_llm(llm, SQL_db, prompt=db_chain_prompt, verbose=True)
+        destination_chains['db_chain'] = db_chain
+
+        dummy_chain_prompt = PromptTemplate(template=self.dummy_prompt_template, input_variables=["query"])
+        dummy_chain = LLMChain(llm=llm, prompt=dummy_chain_prompt)
+        destination_chains['dummy_chain'] = dummy_chain
+
+        default_chain = ConversationChain(llm=llm, output_key="text")
+
+
+        destinations = [f"{p['name']}: {p['description']}" for p in self.prompt_infos]
+        destinations_str = "\n".join(destinations)
+        router_template = MULTI_PROMPT_ROUTER_TEMPLATE.format(destinations=destinations_str)
+        router_prompt = PromptTemplate(
+            template=router_template,
+            input_variables=["input"],
+            output_parser=RouterOutputParser(),
+        )
+        router_chain = LLMRouterChain.from_llm(llm, router_prompt)
+
+        # 에러 발생 : MultiPromptChain의 destination_chain이 SQLDatabaseChain을 지원하지 않는 듯
+        chain = MultiPromptChain(
+            router_chain=router_chain,
+            destination_chains=destination_chains,
+            default_chain=default_chain,
+            verbose=True,
+        )
+        print(chain)
+        return chain(self.query)['result']
--- chat/views.py
+++ chat/views.py
@@ -3,6 +3,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 from django.http import StreamingHttpResponse
 from .rag import RAG
+from time import sleep
 
 import my_settings
 import openai
@@ -18,27 +19,37 @@ class ChatView(View):
         # Parse the request body and extract the prompt
         query = json.loads(request.body)['content']
 
-        generated_sentence = RAG(query, db_info=my_settings.RAG_DB_INFO).get_SQLDatabaseChain_result()
+        try:
+            generated_sentence = RAG(query=query).get_sentence()
 
+            # Define a generator function to stream the response
+            def generate_response():
+                prompt = "translate following sentence in Korean {}".format(generated_sentence)
+                for chunk in openai.ChatCompletion.create(
+                        model="gpt-3.5-turbo",
+                        messages=[{
+                            "role": "user",
+                            "content": prompt
+                        }],
+                        stream=True,
+                ):
 
-        # Define a generator function to stream the response
-        def generate_response():
-            prompt = "translate following sentence in Korean {}".format(generated_sentence)
-            for chunk in openai.ChatCompletion.create(
-                    model="gpt-3.5-turbo",
-                    messages=[{
-                        "role": "user",
-                        "content": prompt
-                    }],
-                    stream=True,
-            ):
+                    content = chunk["choices"][0].get("delta", {}).get("content")
+                    if content is not None:
+                        yield content
 
-                content = chunk["choices"][0].get("delta", {}).get("content")
-                if content is not None:
-                    yield content
+    #        Return a streaming response to the client
+            return StreamingHttpResponse(generate_response(), content_type="application/octet-stream")
 
-#        Return a streaming response to the client
-        return StreamingHttpResponse(generate_response(), content_type="application/octet-stream")
+        except Exception :
+            def generate_err_msg():
+                err_msg = "죄송합니다. 아직 지원하지 않는 유형의 질문입니다."
+                for tok in err_msg:
+                    sleep(0.02) # 스트리밍의 효과를 주기 위해 ...
+                    yield tok
+
+            return StreamingHttpResponse(generate_err_msg(), content_type="application/octet-stream")
+
 
         # Return a JSON error if the request method is not POST
         return JsonResponse({'error': 'Method not allowed.'}, status=405)
